bb940fb2017d104eab032a78764da68846425eed
2DevGs <dev.gustavoserrano@gmail.com>
2025-05-05 17:02:34 -0300

    Implementing Pet Shop List Layout
    In this commit, the List tab structure was implemented to present the data to the user.

--- lib/app/modules/home/widgets/home_supplier_tab.dart
+++ lib/app/modules/home/widgets/home_supplier_tab.dart
@@ -12,16 +12,18 @@ class _HomeSupplierTab extends StatelessWidget {
         _HomeTabHeader(
           homeController: homeController,
         ),
-        Observer(
-          builder: (_) {
-            return AnimatedSwitcher(
-              duration: const Duration(milliseconds: 400),
-              child: homeController.supplierPageTypeSelected ==
-                      SupplierPageType.list
-                  ? const _HomeSupplierList()
-                  : const _HomeSupplierGrid(),
-            );
-          },
+        Expanded(
+          child: Observer(
+            builder: (_) {
+              return AnimatedSwitcher(
+                duration: const Duration(milliseconds: 400),
+                child: homeController.supplierPageTypeSelected ==
+                        SupplierPageType.list
+                    ? const _HomeSupplierList()
+                    : const _HomeSupplierGrid(),
+              );
+            },
+          ),
         ),
       ],
     );
@@ -80,11 +82,118 @@ class _HomeTabHeader extends StatelessWidget {
 }
 
 class _HomeSupplierList extends StatelessWidget {
-  const _HomeSupplierList({super.key});
+  const _HomeSupplierList();
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Supplier List');
+    return CustomScrollView(
+      slivers: [
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            childCount: 5,
+            (context, index) {
+              return _HomeSupplierListItemWidget();
+            },
+            ) 
+        ),
+      ],
+    );
+  }
+}
+
+class _HomeSupplierListItemWidget extends StatelessWidget {
+  const _HomeSupplierListItemWidget();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+      child: Stack(
+        children: [
+          Container(
+            margin: EdgeInsets.only(left: 30),
+            width: 1.sw,
+            height: 80.h,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 50),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          'Clinica Central ABC',
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(
+                           height: 10,
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.location_on,
+                              size: 16,
+                            ),
+                            Text(
+                              '1.34 Km de Distancia',
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: CircleAvatar(
+                    backgroundColor: context.primaryColor,
+                    maxRadius: 15,
+                    child: Icon(
+                      Icons.arrow_forward_ios,
+                      color: Colors.white,
+                      size: 15,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 5),
+            width: 70,
+            height: 70,
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+              border: Border.all(
+                color: Colors.transparent,
+                width: 1,
+              ),
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey[100]!,
+                  width: 5,
+                ),
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(100),
+                image: const DecorationImage(
+                  image: NetworkImage('https://th.bing.com/th/id/OIP.9nSZHSGjFNEAuNOqjeNlNwHaE8?w=283&h=188&c=7&r=0&o=5&pid=1.7'),
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
 
