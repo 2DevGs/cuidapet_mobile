1ea5a7d4ce9fd004f918aabbcc74ce2fea4e9b47
2DevGs <dev.gustavoserrano@gmail.com>
2025-05-06 17:12:54 -0300

    Creating Location Detail Module
    In this commit, the module for the details of the establishment selected by the user was created.

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -3,6 +3,7 @@ import 'package:cuidapet_mobile/app/modules/address/address_module.dart';
 import 'package:cuidapet_mobile/app/modules/auth/auth_module.dart';
 import 'package:cuidapet_mobile/app/modules/core/core_module.dart';
 import 'package:cuidapet_mobile/app/modules/home/home_module.dart';
+import 'package:cuidapet_mobile/app/modules/supplier/supplier_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppModule extends Module {
@@ -21,6 +22,7 @@ class AppModule extends Module {
     r.module('/auth', module: AuthModule());
     r.module('/home', module: HomeModule());
     r.module('/address', module: AddressModule());
+    r.module('/supplier', module: SupplierModule());
     super.routes(r);
   }
 }
